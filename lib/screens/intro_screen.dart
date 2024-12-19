fc7c74f8abda31593a1b2730997ab5b6e2263b28
mohamedhamouda7897 <mohamed.hamouda7897@gmail.com>
2024-12-19 20:16:05 +0200

    add IntroductionScreen

---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class IntroductionScreen extends StatelessWidget {
+  static const String routeName = "introScreen";
+
+  const IntroductionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
